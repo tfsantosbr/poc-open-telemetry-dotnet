421dcd8a85dbce7fc6affdd643d19e8558ea1366
Tiago Santos <tfsantosbr@gmail.com>
2024-09-04 20:06:26 -0300

    remove open telemetry packages

--- src/Orders.Api/Program.cs
+++ src/Orders.Api/Program.cs
@@ -1,17 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 using Orders.Api.Application.CreateOrder;
 using Orders.Api.Metrics;
 using Serilog;
-using OpenTelemetry.Logs;
-using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
-
-const string serviceName = "orders-api";
-var serviceNamespace = builder.Environment.EnvironmentName;
-var serviceInstance = Environment.MachineName;
 
 // Application Dependencies
 
@@ -23,48 +15,9 @@ builder.Services.AddSingleton<OrderMetrics>();
 builder.Services.AddSerilog((services, logger) => logger
     .MinimumLevel.Information()
     .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .WriteTo.OpenTelemetry(options =>
-    {
-        options.Endpoint = "http://host.docker.internal:4317";
-        options.ResourceAttributes = new Dictionary<string, object>
-        {
-            ["service.name"] = serviceName,
-            ["service.namespace"] = serviceNamespace,
-            ["service.instance.id"] = serviceInstance
-        };
-    })
-);
+    .WriteTo.Console());
 
 // ========================================================================================================
-
-// OPEN TELEMETRY =========================================================================================
-
-
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(serviceName))
-    .WithTracing(tracing => tracing
-        .AddSource(OrderMetrics.ActivitySourceName)
-        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
-        .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddOtlpExporter(options =>
-            options.Endpoint = new Uri("http://host.docker.internal:4317")
-        )
-    )
-    .WithMetrics(metrics => metrics
-        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
-        .AddMeter(OrderMetrics.MeterName)
-        .AddAspNetCoreInstrumentation()
-        .AddRuntimeInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddProcessInstrumentation()
-        .AddOtlpExporter(options =>
-            options.Endpoint = new Uri("http://host.docker.internal:4317")
-        )
-    );
-
-// =======================================================================================================
 
 var app = builder.Build();
 
--- src/Orders.Consumer/Program.cs
+++ src/Orders.Consumer/Program.cs
@@ -1,11 +1,6 @@
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 using Orders.Consumer;
 using Orders.Consumer.Metrics;
-using OpenTelemetry.Metrics;
 using Serilog;
-
-const string serviceName = "orders-consumer";
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -17,44 +12,9 @@ builder.Services.AddSingleton<OrdersConsumerMetrics>();
 builder.Services.AddSerilog((services, logger) => logger
     .MinimumLevel.Information()
     .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .WriteTo.OpenTelemetry(options =>
-    {
-        options.Endpoint = "http://host.docker.internal:4317";
-        options.ResourceAttributes = new Dictionary<string, object>
-        {
-            ["service.name"] = serviceName
-        };
-    })
-);
+    .WriteTo.Console());
 
 // ========================================================================================================
-
-// OPEN TELEMETRY =========================================================================================
-
-
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(serviceName))
-    .WithTracing(tracing => tracing
-        .AddSource(OrdersConsumerMetrics.ActivitySourceName)
-        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
-        .AddHttpClientInstrumentation()
-        .AddOtlpExporter(options =>
-            options.Endpoint = new Uri("http://host.docker.internal:4317")
-        )
-    )
-    .WithMetrics(metrics => metrics
-        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
-        .AddMeter(OrdersConsumerMetrics.MeterName)
-        .AddRuntimeInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddProcessInstrumentation()
-        .AddOtlpExporter(options =>
-            options.Endpoint = new Uri("http://host.docker.internal:4317")
-        )
-    );
-
-// =======================================================================================================
 
 var host = builder.Build();
 
